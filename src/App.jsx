b250505b5318240ffa6064f172b992458442bb24
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-04-08 21:34:56 +0530

    notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from "./components/Navbar";
+import Notes from './components/Notes';
 
 
 export default function App() {
   return (
     <>
     <Navbar />
+    <Notes />
     </>
   )
 }
